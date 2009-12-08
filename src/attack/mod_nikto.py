b6144ce240df2652ce18d1d59359960e7adb197d
devloop <devloop@6f1da90a-7915-0410-b0c8-fe3d2c9d8697>
2009-12-08 19:05:28 +0000

    [mod_nikto] Fixed the bug with re-interpretation in nikto csv format.             Added links to OSVDB database in the output.

--- src/attack/mod_nikto.py
+++ src/attack/mod_nikto.py
@@ -21,10 +21,9 @@ class mod_nikto(Attack):
 
   def __init__(self, HTTP, xmlRepGenerator):
     Attack.__init__(self, HTTP, xmlRepGenerator)
-    csv.register_dialect("nikto", quoting=csv.QUOTE_ALL, doublequote=False, escapechar="\\")
     try:
       fd = open(self.CONFIG_DIR + "/" + self.CONFIG_FILE)
-      reader = csv.reader(fd, "nikto")
+      reader = csv.reader(fd)
       self.nikto_db = [l for l in reader if l!=[] and l[0].isdigit()]
       fd.close()
     except IOError:
@@ -32,12 +31,13 @@ class mod_nikto(Attack):
         print "Problem with local nikto database."
         print "Downloading from the web..."
         page = urllib2.urlopen("http://cirt.net/nikto/UPDATES/2.1.0/db_tests")
+        csv.register_dialect("nikto", quoting=csv.QUOTE_ALL, doublequote=False, escapechar="\\")
         reader = csv.reader(page, "nikto")
         self.nikto_db = [l for l in reader if l!=[] and l[0].isdigit()]
         page.close()
 
         fd = open(self.CONFIG_DIR + "/" + self.CONFIG_FILE, "w")
-        writer = csv.writer(fd, "nikto")
+        writer = csv.writer(fd)
         writer.writerows(self.nikto_db)
         fd.close()
       except IOError:
@@ -48,67 +48,75 @@ class mod_nikto(Attack):
       match = match_or = match_and = False
       fail = fail_or = False
 
+      l[3] = l[3].replace("@CGIDIRS","/cgi-bin/")
+      l[3] = l[3].replace("@ADMIN","/admin/")
+      if l[3][0] == "@":
+        continue
+      if l[3][0] != "/":
+        l[3] = "/" + l[3]
+
       if l[4] == "GET":
-        l[3] = l[3].replace("@CGIDIRS","/cgi-bin/")
-        l[3] = l[3].replace("@ADMIN","/admin/")
-        if l[3][0] == "@":
-          continue
-        if l[3][0] != "/":
-          l[3] = "/" + l[3]
         resp = self.HTTP.send("http://"+self.HTTP.server+l[3])
-        page, code = resp.getPageCode()
-        raw = " ".join([x + ": " + y for x,y in resp.getInfo().items()])
-        raw += page
+      elif l[4] == "POST":
+        resp = self.HTTP.send("http://"+self.HTTP.server+l[3], l[11])
+      else:
+        resp = self.HTTP.send("http://"+self.HTTP.server+l[3], l[11], method=l[4])
 
-        # First condition (match)
-        if len(l[5]) == 3 and l[5].isdigit():
-          if code == l[5]:
-            match = True
+      page, code = resp.getPageCode()
+      raw = " ".join([x + ": " + y for x,y in resp.getInfo().items()])
+      raw += page
+
+      # First condition (match)
+      if len(l[5]) == 3 and l[5].isdigit():
+        if code == l[5]:
+          match = True
+      else:
+        if raw.find(l[5]) > -1:
+          match = True
+
+      # Second condition (or)
+      if l[6] != "":
+        if len(l[6]) == 3 and l[6].isdigit():
+          if code == l[6]:
+            match_or = True
         else:
-          if raw.find(l[5]) > -1:
-            match = True
+          if raw.find(l[6]) > -1:
+            match_or = True
 
-        # Second condition (or)
-        if l[6] != "":
-          if len(l[6]) == 3 and l[6].isdigit():
-            if code == l[6]:
-              match_or = True
-          else:
-            if raw.find(l[6]) > -1:
-              match_or = True
-
-        # Third condition (and)
-        if l[7] != "":
-          if len(l[7]) == 3 and l[7].isdigit():
-            if code == l[7]:
-              match_and = True
-          else:
-            if raw.find(l[7]) > -1:
-              match_and = True
+      # Third condition (and)
+      if l[7] != "":
+        if len(l[7]) == 3 and l[7].isdigit():
+          if code == l[7]:
+            match_and = True
         else:
-          match_and = True
+          if raw.find(l[7]) > -1:
+            match_and = True
+      else:
+        match_and = True
 
-        # Fourth condition (fail)
-        if l[8] != "":
-          if len(l[8]) == 3 and l[8].isdigit():
-            if code == l[8]:
-              fail = True
-          else:
-            if raw.find(l[8]) > -1:
-              fail = True
+      # Fourth condition (fail)
+      if l[8] != "":
+        if len(l[8]) == 3 and l[8].isdigit():
+          if code == l[8]:
+            fail = True
+        else:
+          if raw.find(l[8]) > -1:
+            fail = True
 
-        # Fifth condition (or)
-        if l[9] != "":
-          if len(l[9]) == 3 and l[9].isdigit():
-            if code == l[9]:
-              fail_or = True
-          else:
-            if raw.find(l[9]) > -1:
-              fail_or = True
+      # Fifth condition (or)
+      if l[9] != "":
+        if len(l[9]) == 3 and l[9].isdigit():
+          if code == l[9]:
+            fail_or = True
+        else:
+          if raw.find(l[9]) > -1:
+            fail_or = True
 
-        if ((match or match_or) and match_and) and not (fail or fail_or):
-          print "http://" + self.HTTP.server + l[3]
-          print l[10]
-          print
+      if ((match or match_or) and match_and) and not (fail or fail_or):
+        print "http://" + self.HTTP.server + l[3]
+        print l[10]
+        if l[1] != "0":
+          print "\thttp://osvdb.org/show/osvdb/" + l[1]
+        print
 
 
