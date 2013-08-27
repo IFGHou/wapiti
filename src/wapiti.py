eb0ab85cfbfcc5bcb7826da4e432cd60128e6d08
devloop <devloop@6f1da90a-7915-0410-b0c8-fe3d2c9d8697>
2013-08-27 19:46:46 +0000

    More work on french translations + fixed a type + remove usage from __doc__ of the Wapiti class (usage will only be in the .po files)

--- src/wapiti.py
+++ src/wapiti.py
@@ -70,96 +70,8 @@ class InvalidOptionValue(Exception):
 
 
 class Wapiti(object):
-    """Usage: python wapiti.py http://server.com/base/url/ [options]
-
-Supported options are:
--s <url>
---start <url>
-    To specify an url to start with
-
--x <url>
---exclude <url>
-    To exclude an url from the scan (for example logout scripts)
-    You can also use a wildcard (*)
-    Example : -x "http://server/base/?page=*&module=test"
-    or -x http://server/base/admin/* to exclude a directory
-
--p <url_proxy>
---proxy <url_proxy>
-    To specify a proxy
-    Exemple: -p http://proxy:port/
-
--c <cookie_file>
---cookie <cookie_file>
-    To use a cookie
-
--t <timeout>
---timeout <timeout>
-    To fix the timeout (in seconds)
-
--a <login%password>
---auth <login%password>
-    Set credentials for HTTP authentication
-
---auth-method <method>
-    Set the authentication method (basic, ntml, digest, kerberos) to use
-
--r <parameter_name>
---remove <parameter_name>
-    Remove a parameter from URLs
-
--n <limit>
---nice <limit>
-    Define a limit of urls to read with the same pattern
-    Use this option to prevent endless loops
-    Must be greater than 0
-
--m <module_options>
---module <module_options>
-    Set the modules and HTTP methods to use for attacks.
-    Example: -m "-all,xss:get,exec:post"
-
--u
---underline
-    Use color to highlight vulnerables parameters in output
-
--v <level>
---verbose <level>
-    Set the verbosity level
-    0: quiet (default), 1: print each url, 2: print every attack
-
--f <type_file>
---reportType <type_file>
-    Set the type of the report
-    xml: Report in XML format
-    html: Report in HTML format
-
--o <output>
---output <output_file>
-    Set the name of the report file
-    If the selected report type is "html", this parameter must be a directory
-
--i <file>
---continue <file>
-    This parameter indicates Wapiti to continue with the scan from the specified
-    file, this file should contain data from a previous scan.
-    The file is optional, if it is not specified, Wapiti takes the default file
-    from \"scans\" folder.
-
--k <file>
---attack <file>
-    This parameter indicates Wapiti to perform attacks without scanning again the
-    website and following the data of this file.
-    The file is optional, if it is not specified, Wapiti takes the default file
-    from \"scans\" folder.
-
---verify-ssl <0|1>
-    This parameter indicates whether Wapiti must check SSL certificates.
-    Default is to verify certificates
-
--h
---help
-    To print this usage message"""
+    """This class parse the options from the command line and set the modules and the HTTP engine accordingly.
+    Launch wapiti without arguments or with the "-h" option for more informations."""
 
     target_url = None
     target_scope = "folder"
@@ -415,7 +327,7 @@ Supported options are:
         self.outputFile = outputFile
 
 if __name__ == "__main__":
-    doc = _("wapityDoc")
+    doc = _("wapitiDoc")
     try:
         prox = ""
         auth = []
